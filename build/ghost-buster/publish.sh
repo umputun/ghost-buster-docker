84fe172bae5ffc008f66e6cb285539431f4fb159
Umputun <umputun@gmail.com>
2015-09-11 10:57:11 -0500

    workarounds for rss and disqus count issues

--- build/ghost-buster/publish.sh
+++ build/ghost-buster/publish.sh
@@ -4,6 +4,11 @@ buster generate
 echo "export md"
 ghost-export /ghost /export.md
 
+# workarounds for buster's issues
+sed -i -e 's|/index.html#disqus_thread|/#disqus_thread|g' /ghost/static/index.html
+curl http://127.0.0.1:2368/rss/ > /ghost/static/rss
+curl http://127.0.0.1:2368/rss/ > /ghost/static/rss.xml
+
 echo "publishing ..."
 #copy from  static/* to external desitnation
 
