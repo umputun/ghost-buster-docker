4533c2e067da980e9437f9ec447d850655a986e6
Umputun <umputun@gmail.com>
2015-09-12 18:03:57 -0500

    fix for disqus comments in tags

--- build/ghost-buster/publish.sh
+++ build/ghost-buster/publish.sh
@@ -6,6 +6,7 @@ ghost-export /ghost /export.md
 
 # workarounds for buster's issues
 sed -i -e 's|/index.html#disqus_thread|/#disqus_thread|g' /ghost/static/index.html
+find /ghost/static/tag -name index.html | xargs sed -i -e 's|/index.html#disqus_thread|/#disqus_thread|g'
 curl http://127.0.0.1:2368/rss/ > /ghost/static/rss
 curl http://127.0.0.1:2368/rss/ > /ghost/static/rss.xml
 
