8a73d3c17e4de8c3f867c4e57c75b23d18f49cc1
Umputun <umputun@gmail.com>
2015-09-20 16:36:35 -0500

    no need to clone repo

--- setup-all.sh
+++ setup-all.sh
@@ -5,16 +5,12 @@ curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose
 chmod +x /tmp/docker-compose
 sudo mv -f /tmp/docker-compose /usr/local/bin/docker-compose
 
-echo "fetch repo from github"
-cd /srv
-git clone https://github.com/umputun/ghost-buster-docker.git
+cd ./ghost-buster-docker
 
 echo "edit params"
-cd /srv/ghost-buster-docker
-nano /srv/ghost-buster-docker/docker-compose.yml
+nano ./ghost-buster-docker/docker-compose.yml
 
 echo "start ghost and nginx"
-cd /srv/ghost-buster-docker
 docker-compose up -d
 
 echo "all done"
